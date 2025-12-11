52f3faa0b16f6d9f1696dfbd58b69d5e505d0da0
Vishal Pandey <contact@vishalpandey.co.in>
2025-12-11 11:56:52 +0000

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
     },
   },
   root: path.resolve(import.meta.dirname, "client"),
-  base: "/Fantasy-Mystery-Quest/",
+  base: "/",
   build: {
     outDir: path.resolve(import.meta.dirname, "docs"),
     emptyOutDir: true,
